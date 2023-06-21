d5235f0cbe0169b1f37e40e2ec8f91fae40b519b
Noel MINET <noel.minet@gmail.com>
2023-06-21 11:25:27 +0700

    Add documentation on MapsAndLists.

--- src/maps_and_lists.rs
+++ src/maps_and_lists.rs
@@ -2,6 +2,59 @@ use std::{collections::HashMap, cell::RefCell, rc::Rc};
 use crate::context::{Context, ContextRef, ContextValue};
 
 
+/// Minimun [Context] implementation.
+/// 
+/// The [MapsAndLists] context is intended as a bridge between application
+/// data and rendering, when access to data can be anticipated thus making
+/// eager copy of source data suitable.
+/// Its also supports mustache lambda wherein the context can emit a template
+/// that must be processed with the current stack.
+/// 
+/// # Sample
+/// 
+/// ```
+/// use mustache::{Template, MapsAndLists};
+/// use std::{collections::HashMap, cell::RefCell, rc::Rc};
+/// 
+/// let source = Rc::from(
+///     "{{#wrapped}}hello {{#names}}{{.}}{{sep}}{{/names}}{{/wrapped}}"
+/// );
+/// let template = Template::from(&source).unwrap();
+/// 
+/// let names = vec![ "john", "paul", "jacques"];
+/// 
+/// // in real life code below would be some form of adapter function
+/// let counter = RefCell::new(names.len() - 1);
+/// let context = MapsAndLists::mapping(
+///   vec![
+///     (String::from("names"), MapsAndLists::sequence(
+///         names.into_iter().map(
+///             |n| MapsAndLists::text(n)
+///         ).collect::<Vec<_>>()
+///     )),
+///     (String::from("sep"), MapsAndLists::lambda0(
+///         move || {
+///             let current = { *counter.borrow() };
+///             if current > 0 {
+///                 counter.replace(current - 1);
+///                 ", ".to_owned()
+///             } else {
+///                 "".to_owned()
+///             }
+///         }
+///     )),
+///     (String::from("wrapped"), MapsAndLists::lambda1(
+///         |s| format!("[{}]", s),
+///         &source)),
+///   ].into_iter().collect::<HashMap<_, _>>()
+/// );
+/// let result = template.render(&context);
+/// 
+/// assert_eq!(result, "[hello john, paul, jacques]")
+/// ```
+
+pub struct MapsAndLists(Value);
+
 enum Value {
     Null,
     Bool(bool),
@@ -11,8 +64,6 @@ enum Value {
     Lambda0(Box<dyn Fn() -> String>, RefCell<String>),
     Lambda1(Box<dyn Fn(&str) -> String>, Rc<str>, RefCell<String>),
 }
-
-pub struct MapsAndLists(Value);
 
 impl MapsAndLists {
     pub fn null() -> MapsAndLists {
@@ -35,6 +86,12 @@ impl MapsAndLists {
         MapsAndLists(Value::Sequence(sequence))
     }
 
+    /// Create a mustache lambda.
+    /// 
+    /// When rendering, `fun` is called each time it is referenced.
+    /// 
+    /// As per mustache specification, a lambda is considered truthy even
+    /// when its result is falsy.
     pub fn lambda0<T>(fun: T) -> MapsAndLists
     where T: Fn() -> String + 'static {
         MapsAndLists(Value::Lambda0(
@@ -43,6 +100,12 @@ impl MapsAndLists {
         ))
     }
 
+    /// Create a mustache lambda processing a section.
+    /// 
+    /// When rendering, `fun` is called each time it is referenced.
+    /// 
+    /// `fun` receives a slice of `template` and must return a new mustache
+    /// text that will be compiled and rendered.
     pub fn lambda1<T>(fun: T, template: &Rc<str>) -> MapsAndLists
     where T: Fn(&str) -> String + 'static {
         MapsAndLists(Value::Lambda1(
@@ -112,6 +175,7 @@ impl<'a> Context<'a> for MapsAndLists{
         match self {
             MapsAndLists(Value::Null) => true,
             MapsAndLists(Value::Bool(b)) => !b,
+            MapsAndLists(Value::Text(t)) => t.is_empty(),
             _ => false
         }
     }
